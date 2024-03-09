262b7e44ab1655555d57495ec75b60ae24deefb4
Stefan Jucker <stefan.jucker@gmail.com>
2024-03-09 19:11:16 +0100

    Modul G

---
+++ src/app/report-score/report-score.component.ts
@@ -0,0 +1,30 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {JudgeReportRatingDTO} from "../rest";
+
+@Component({
+  selector: 'app-report-score',
+  templateUrl: './report-score.component.html',
+  styleUrl: './report-score.component.scss'
+})
+export class ReportScoreComponent {
+
+  @Input({required: true})
+  rating!: JudgeReportRatingDTO;
+
+  @Input({required: true})
+  readOnly = false;
+
+  @Input({required: true})
+  min = 0;
+
+  @Input({required: true})
+  max = 20;
+
+  @Output()
+  changed = new EventEmitter<void>();
+
+  onChange(): void {
+    this.changed.emit();
+  }
+
+}
